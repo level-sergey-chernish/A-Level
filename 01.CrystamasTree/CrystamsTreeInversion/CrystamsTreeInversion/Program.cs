a433e38c7e1ebee3f88ca9f9276687fbdef145ad
Sergey Chernish <sergey.chernish@gmail.com>
2018-07-25 19:15:19 +0300

    Add HW 01

---
+++ 01.CrystamasTree/CrystamsTreeInversion/CrystamsTreeInversion/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CrystmasTreeInversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Size of the base of the pyramid
+            byte maxLength = 9;
+            byte maxHeight = Convert.ToByte(maxLength*2/3);
+            //Cycle counters
+            byte x, y;
+            //Meddle of the string
+            byte mediana = Convert.ToByte(maxLength / 2); ;
+            //Strings of the output results
+            string s1 = String.Empty;
+
+            System.Console.WriteLine("MAGIC HERE:\n\n\noutput:");
+
+            // Using "Fibonachi" algo for finding dots between asterisks
+            for (x = 0; x < maxHeight; x++)
+            {
+                for (y = 0; y < maxLength; y++)
+                {
+                    if ((y > (mediana - x)) &
+                        (y < (mediana + x)) &
+                        (x != (maxHeight - 1)))
+                    {
+                        s1 += ".";
+                    }
+                    else
+                    {
+                        s1 += "*";
+                    }
+                     
+                }
+                System.Console.WriteLine(s1);
+                s1 = String.Empty;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 01.CrystamasTree/CrystmasTree/CrystmasTree/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CrystmasTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Size of the base of the pyramid
+            byte maxLength = 6;
+            //Cycle counters
+            byte x, y, z;
+            //Strings of the output results
+            string s1 = "";
+            string s2 = string.Empty;
+
+            /* Logic: in every cycle form two string s1 and s2. s1-length on every step of the cycle
+            decrease on 1, s2-length on every step of the cycle increase on 1 and concatenate s1 and s2 with deletion of
+            the last symbol */
+            System.Console.WriteLine("MAGIC HERE:\n\n\noutput:");
+            for (x = maxLength; x > 0; x--)
+            {
+                for (y = x; y > 1; y--)
+                {
+                    s1 += " ";
+                }
+                for (z = 0; z <= (maxLength - x); z++)
+                {
+                    s2 += "*.";
+                }
+                Console.WriteLine((s1 + s2).Remove(((s1 + s2).Length - 1), 1));
+                s1 = String.Empty;
+                s2 = String.Empty;
+            }
+        }
+    }
+}
