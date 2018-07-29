0a1cccd409bfb581d664c5bbccadde7b8c187542
Sergey Chernish <sergey.chernish@gmail.com>
2018-07-29 22:06:26 +0300

    Add prototype of Students App (links one to multi e.g. class Groups)

---
+++ 10.Students/Students/tutor/tutor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students
+{
+    public class Tutor
+    {
+        public string Name { get; set; }
+        public Degree Degree;
+    }
+
+    public enum Degree
+    {
+        Professor,
+        Docent,
+        Assistant
+    }
+}
