722d15094370b5832a4f3adf3e4911a7183357be
Soft Micro <sergey.chernish@yahoo.com>
2018-08-25 10:14:32 +0300

    Add HW 16 (Generics, Reflexion)

---
+++ 16.HW/Reflexion/Reflexion/Func.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflexion
+{
+    internal class Func
+    {
+        internal static void PrintObjProperties<T>(T obj)
+        {
+
+            Console.WriteLine(new string('-', 82));
+
+            Console.WriteLine("| {0, -20} | {1, -20} | {2, -32} |", "Type", "Name", "Value");
+
+            Console.WriteLine(new string('-', 82));
+
+            foreach (var property in obj.GetType().GetProperties())
+            {
+              
+                Console.WriteLine("| {0, -20} | {1, -20} | {2, -32} |", 
+                    property.PropertyType.Name, property.Name, property.GetValue(obj));
+
+            }
+
+            Console.WriteLine(new string('-', 82));
+        }
+
+    }
+}
---
+++ 16.HW/Reflexion/Reflexion/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflexion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person = new Person()
+            {
+                FirstName = "Axell",
+
+                LastName = "Havoc",
+
+                Age = 38,
+
+                Email = "Axell@orient.suo",
+
+                Password = "Axell'sPassword",
+
+                CardInfo = new BankCardInfo()
+                {
+                    Cvv = "132",
+
+                    Pin = "354",
+
+                    CardName = "Axell Havoc",
+
+                    CardNumber = "1234 5678 0987 6543"
+                }
+            };
+
+            Func.PrintObjProperties(person);
+        }
+
+
+    }
+}
