5dcd930f9b09dbf12be75f281d7121dee5fea2b4
Sergey Chernish <sergey.chernish@gmail.com>
2018-09-10 11:27:56 +0300

    Add cockroachrun HW 19

---
+++ 19.CockroachRun/CochroachRun/Classes/Cockroach.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CockroachRun.Classes
+{
+    class Cockroach : Runner
+    {
+        internal Cockroach(char appearence) : base(appearence)
+        {
+        }
+    }
+}
